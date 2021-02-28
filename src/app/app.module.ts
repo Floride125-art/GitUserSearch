d2d0bac12a2a344421d7b406a00d038909f8e66d
Floride125-art <FTuyisenge120@daviscollege.com>
2021-02-28 12:25:46 +0200

    imported some files in app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,42 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RequestService} from './request.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import { NavComponent } from './nav/nav.component';
-import { UserComponent } from './user/user.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
+import { FormComponent } from './form/form.component';
+import { UserComponent } from './user/user.component';
+import { RouterModule } from '@angular/router';
+import { RoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { Upper } from './upper.pipe';
 import { HoverDirective } from './hover.directive';
-import { UpperPipe } from './upper.pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     NavComponent,
-    UserComponent,
     RepositoriesComponent,
-    HoverDirective,
-    UpperPipe
+    FormComponent,
+    UserComponent,
+    Upper,
+    HoverDirective
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RoutingModule,
+    RouterModule,
+    FormsModule,
+    HttpClientModule,
+  
+
   ],
-  providers: [],
+  providers: [RequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
