226b6a02efb51866f542c80736a349419e1761a2
Floride125-art <FTuyisenge120@daviscollege.com>
2021-02-28 02:42:08 +0200

    create a component nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
