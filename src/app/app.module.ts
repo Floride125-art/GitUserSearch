e483bd5fda481466e5bdbf615f91b90c4e3dfb9a
Floride125-art <FTuyisenge120@daviscollege.com>
2021-02-28 02:43:09 +0200

    create a component user for getting usernames of users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    NavComponent
+    NavComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
